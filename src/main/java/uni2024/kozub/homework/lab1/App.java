047413a3f9d0b0da51bf07c03acad4e5889530b2
Kostya <kostyakozyb@gmail.com>
2024-11-02 15:31:08 +0200

    Lab 5 was updated

--- src/main/java/uni2024/kozub/homework/lab1/App.java
+++ src/main/java/uni2024/kozub/homework/lab1/App.java
@@ -2,22 +2,23 @@ package uni2024.kozub.homework.lab1;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
 
         UserDAO userDAO = new UserDAO();
-//
-//        // Створення нового користувача
-//        User newUser = new User();
-//        newUser.setName("Kostya Kozub");
-//        newUser.setEmail("kk@example.com");
-//        newUser.setPassword("p1234");
-//        newUser.setAge(19);
-//        newUser.setAddress("456 Main St");
-//
-//        userDAO.createUser(newUser);
-//        System.out.println("Користувача створено: " + newUser);
+
+        // Створення нового користувача
+        User newUser = new User();
+        newUser.setName("Kostya Kozub");
+        newUser.setEmail("kk@example.com");
+        newUser.setPassword("p1234");
+        newUser.setAge(19);
+        newUser.setAddress("456 Main St");
+
+        userDAO.createUser(newUser);
+        System.out.println("Користувача створено: " + newUser);
 
 //         Отримання користувача за ID
 
@@ -28,8 +29,8 @@ public class App {
 //        } else {
 //            System.out.println("Користувача з ID " + userId + " не знайдено.");
 //        }
-//
-        // Оновлення даних користувача
+
+//         Оновлення даних користувача
 
 //        User retrievedUser = userDAO.getUserById(3);
 //        if (retrievedUser != null) {
@@ -43,8 +44,8 @@ public class App {
 //        } else {
 //            System.out.println("Користувача з ID " + 3 + " не знайдено.");
 //        }
-//
-      // Отримання всіх користувачів
+
+//       Отримання всіх користувачів
 
 //        List<User> users = userDAO.getAllUsers();
 //        System.out.println("Список всіх користувачів:");
@@ -52,7 +53,7 @@ public class App {
 //            System.out.println(user);
 //        }
 
-        // Видалення користувача
+//         Видалення користувача
 
 //        userDAO.deleteUser(2);
 //        System.out.println("Користувача видалено з ID: " + 2);
@@ -61,63 +62,63 @@ public class App {
 
         // Lab 4
 
-        GameBuilder builder = new GameBuilder();
-        builder.setName("CS:GO");
-        builder.setDescription("None");
-        builder.setGenre(Genre.ACTION);
-        builder.setPlatform(Platform.PC);
-        builder.setAgeRestriction(18);
-        builder.setRating(4.0);
-        builder.setPrice(10.5);
-        builder.setReleaseDate(LocalDate.of(2010, 3, 1));
-
-        Game game1 = null;
-        try {
-            game1 = builder.build();
-            System.out.println("Гра створена успішно: " + game1);
-        } catch (IllegalArgumentException e) {
-            System.err.println("Помилки валідації: " + e.getMessage());
-        }
-
-        UserBuilderImpl userBuilder = new UserBuilderImpl();
-        User user1 = null;
-
-        try {
-            user1 = userBuilder
-                    .setId(1) // Ви можете задати ID, якщо потрібно
-                    .setName("K")
-                    .setEmail("kostyakozyb@gmail.com")
-                    .setPassword("123456789")
-                    .setAge(18)
-                    .setAddress("Toporivtsi")
-                    .setRole(Role.CUSTOMER) // Можна задати роль, якщо потрібно
-                    .build();
-
-            // Додаємо гру до списку бажаних
-            if (game1 != null) {
-                user1.addGameToWishList(game1);
-            }
-
-            System.out.println(user1);
-        } catch (IllegalArgumentException e) {
-            System.err.println("Помилки валідації: " + e.getMessage());
-        }
-
-        builder.setName("A very long name exceeding the maximum limit of characters allowed in this field"); // порушує @Size
-        builder.setDescription(null);
-        builder.setGenre(null);
-        builder.setPlatform(null);
-        builder.setPrice(-10.0);
-        builder.setRating(10.0);
-        builder.setAgeRestriction(25);
-        builder.setReleaseDate(LocalDate.now().plusDays(1));
-
-        try {
-            Game game = builder.build();
-            System.out.println("Гра створена успішно: " + game);
-        } catch (IllegalArgumentException e) {
-            System.err.println("Помилки валідації: " + e.getMessage());
-        }
+//        GameBuilder builder = new GameBuilder();
+//        builder.setName("CS:GO");
+//        builder.setDescription("None");
+//        builder.setGenre(Genre.ACTION);
+//        builder.setPlatform(Platform.PC);
+//        builder.setAgeRestriction(18);
+//        builder.setRating(4.0);
+//        builder.setPrice(10.5);
+//        builder.setReleaseDate(LocalDate.of(2010, 3, 1));
+//
+//        Game game1 = null;
+//        try {
+//            game1 = builder.build();
+//            System.out.println("Гра створена успішно: " + game1);
+//        } catch (IllegalArgumentException e) {
+//            System.err.println("Помилки валідації: " + e.getMessage());
+//        }
+//
+//        UserBuilderImpl userBuilder = new UserBuilderImpl();
+//        User user1 = null;
+//
+//        try {
+//            user1 = userBuilder
+//                    .setId(1) // Ви можете задати ID, якщо потрібно
+//                    .setName("K")
+//                    .setEmail("kostyakozyb@gmail.com")
+//                    .setPassword("123456789")
+//                    .setAge(18)
+//                    .setAddress("Toporivtsi")
+//                    .setRole(Role.CUSTOMER) // Можна задати роль, якщо потрібно
+//                    .build();
+//
+//            // Додаємо гру до списку бажаних
+//            if (game1 != null) {
+//                user1.addGameToWishList(game1);
+//            }
+//
+//            System.out.println(user1);
+//        } catch (IllegalArgumentException e) {
+//            System.err.println("Помилки валідації: " + e.getMessage());
+//        }
+//
+//        builder.setName("A very long name exceeding the maximum limit of characters allowed in this field"); // порушує @Size
+//        builder.setDescription(null);
+//        builder.setGenre(null);
+//        builder.setPlatform(null);
+//        builder.setPrice(-10.0);
+//        builder.setRating(10.0);
+//        builder.setAgeRestriction(25);
+//        builder.setReleaseDate(LocalDate.now().plusDays(1));
+//
+//        try {
+//            Game game = builder.build();
+//            System.out.println("Гра створена успішно: " + game);
+//        } catch (IllegalArgumentException e) {
+//            System.err.println("Помилки валідації: " + e.getMessage());
+//        }
 
 
 
